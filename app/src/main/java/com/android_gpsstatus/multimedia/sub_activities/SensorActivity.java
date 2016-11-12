7d2c78f43e17aa213ea05aa4e4884d3612078c19
Marcin Mackiewicz <m.mackiewicz@kainos.com>
2016-11-12 12:43:56 +0100

    Added sensor view

--- app/src/main/java/com/android_gpsstatus/multimedia/sub_activities/SensorActivity.java
+++ app/src/main/java/com/android_gpsstatus/multimedia/sub_activities/SensorActivity.java
@@ -1,15 +1,67 @@
 package com.android_gpsstatus.multimedia.sub_activities;
 
+import android.graphics.Color;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.android_gpsstatus.multimedia.R;
 
-public class SensorActivity extends AppCompatActivity {
+import java.util.Arrays;
+import java.util.List;
+
+public class SensorActivity extends AppCompatActivity implements SensorEventListener {
+    private SensorManager sensorManager;
+    private boolean color = false;
+    private long lastUpdate;
+
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+        if (event.sensor.getType() == Sensor.TYPE_ACCELEROMETER) {
+            getAccelerometer(event);
+        }
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+        Log.i("Sensor", "Data: " + sensor.getName());
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        sensorManager.registerListener(this,
+                sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER),
+                SensorManager.SENSOR_DELAY_NORMAL);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sensor);
+
+        sensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
+        lastUpdate = System.currentTimeMillis();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        sensorManager.unregisterListener(this);
+    }
+
+    private void getAccelerometer(SensorEvent event) {
+        ((TextView)findViewById(R.id.xTextView)).setText(String.valueOf(event.values[0]));
+        ((TextView)findViewById(R.id.yTextView)).setText(String.valueOf(event.values[1]));
+        ((TextView)findViewById(R.id.zTextView)).setText(String.valueOf(event.values[2]));
     }
 }
